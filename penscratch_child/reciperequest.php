94774f5a315e9941bee0ca82c445c17f1b0cac78
vkinnealey <victoria.kinnealey@gmail.com>
2017-07-29 12:52:52 -0400

    Updates to recipe request form
    created recipe request form using theme rather than plugin

---
+++ penscratch_child/reciperequest.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Thank you for your recipe request <?php echo $_POST["name"];?><br><br>
+
+To go back to the main page click here <button onclick = "history.go(-1);">Back </button>
+
+</body>
+</html>
