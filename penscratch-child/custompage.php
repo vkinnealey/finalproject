d7bbf7b53f7b5d7f6d43b77bdf7fbfb057d2455a
vkinnealey <victoria.kinnealey@gmail.com>
2017-08-03 22:45:07 -0400

    updates to child theme (form) and slideshow
    trying to get child theme to show on live site and working to create a functioning slideshow feature.

---
+++ penscratch-child/custompage.php
@@ -0,0 +1,73 @@
+<?php /*Template Name: CustomForm 
+	Description: Penscratch Child Theme
+	Author: Victoria Kinnealey
+	Template: penscratch
+	*/?>
+
+<!DOCTYPE html>
+<html>
+
+<?php wp_head();?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-branding">
+			<?php if ( function_exists( 'jetpack_the_site_logo' ) ) jetpack_the_site_logo(); ?>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		</div>
+
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<button class="menu-toggle"><?php _e( 'Menu', 'penscratch' ); ?></button>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
+		<?php if ( get_header_image() ) : ?>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+				<img class="custom-header" src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+			</a>
+		<?php endif;  // End header image check. ?>
+
+<!-- PERSONALIZED FORM CODE-->
+
+<h2>If you would like to request a recipe, please enter your information below and click the "Email Me!" button! </h2>
+
+<form name="Request a Recipe" method="POST" 
+onsubmit="return" action="mailto:victoriakinnealey@gmail.com" enctype="multipart/form-data">
+Your Name <input value="Name" type="text" name="Name"/><br/><br/>
+Recipe Request <input value="Recipe" type="text" name="RecipeRequest"/><br/><br/>
+Allergies <input value="Allergies" type="text" name="Allergies"/><br/><br/>
+Email <input value="Email" type="text" name="Email"/><br/><br/>
+<input type="Submit" value="Email Me!"/>
+</form>
+<form action="reciperequest.php" method="get>"
+
+<?php
+//form components
+$name= $_POST["name"];
+$recipe_request=$_POST["recipe_request"];
+$allergies=$_POST["allergies"];
+$email=$_POST["email"];
+?>
+
+<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'penscratch' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'penscratch' ), 'WordPress' ); ?></a>
+			<span class="sep"> ~ </span>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'penscratch' ), 'Penscratch', '<a href="http://wordpress.com/themes/penscratch/" rel="designer">WordPress.com</a>' ); ?>
+		</div><!-- .site-info -->
+		<?php if ( has_nav_menu ( 'social' ) ) : ?>
+			<?php wp_nav_menu( array( 'theme_location' => 'social', 'depth' => 1, 'link_before' => '<span class="screen-reader-text">', 'link_after' => '</span>', 'container_class' => 'social-links', ) ); ?>
+		<?php endif; ?>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
+
--- slideshow_plugin_updated/slideshow_plugin_updated.php
+++ slideshow_plugin_updated/slideshow_plugin_updated.php
@@ -4,7 +4,7 @@
     <title>
     <?php
     /*
-      Plugin Name: Slideshow Updated
+      Plugin Name: Slideshow 
       Description: Simple slideshow plugin for WordPress site -- created using https://code.tutsplus.com/tutorials/build-a-slideshow-plugin-for-wordpress--wp-25789
       Author: Victoria Kinnealey
       Version: 1.0
@@ -14,72 +14,76 @@
   </head>
 <body>
 <?php
-/*
 function np_init() {
-    add_shortcode("np-shortcode", "np_function");
-    $slide = array(
+  add_shortcode('np-shortcode', 'np_function');
+    $args = array(
       "public" => true,
       "label" => "slides",
       "supports" => array(
         "title",
         "thumbnail",
-        "author"
         )
        );
-    register_post_type("slides", $args);
+    register_post_type("np_images", $args);
   }
-  add_action("init", "np_init");
+add_action("init", "np_init");
+
+add_action('wp_print_scripts', 'np_register_scripts');
+add_action('wp_print_styles', 'np_register_styles');
+
+add_image_size('np_widget', 180, 100, true);
+add_image_size('np_function', 600, 280, true);
+add_theme_support( 'post-thumbnails' );
   
-//custom fields
-//not sure if these are placed correctly, but sizes images
-    add_image_size("np_widget", 180, 100, true);
-    add_image_size("np_function", 600, 280, true);
-    register_post_type("slide_images", $slide);
-  }
-  add_theme_support("post-thumbnails");
-  add_action("init", "np_init");
-
-//unclear what this code does... 
-  function np_function ($type="np_function"){
-    $slide = array(
-      "post_type" => "slide images",
-      "posts_per_page" => 3
+function np_function($type='np_function') {
+    $args = array(
+        'post_type' => 'np_images',
+        'posts_per_page' => 5
     );
-    $result = "<div class='slider-wrapper theme-default'>";
-    $result .= "<div id='slider' class='slider'>";
-
-  //creates actual slideshow loop functionality
-    $loop = new WP_Query($slide);
-    while ($loop->have_posts()){
-      $loop->the_post();
-
-      $the_url= wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), $type);
-      $result='<img title="'.get_the_title().'"src="'. $the_url[0].'"data-thumb="'. $the_url[0] .'"alt=""/>';
+    $result = '<div class="slider-wrapper theme-default">';
+    $result .= '<div id="slider" class="nivoSlider">';
+ 
+    //the loop
+    $loop = new WP_Query($args);
+    while ($loop->have_posts()) {
+        $loop->the_post();
+ 
+        $the_url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), $type);
+        $result .='<img title="'.get_the_title().'" src="' . $the_url[0] . '" data-thumb="' . $the_url[0] . '" alt=""/>';
     }
-  }
-  //creating the Widget
-  function np_widgets_init(){
-    register_widget("np_Widget");
-  }
+    $result .= '</div>';
+    $result .='<div id = "htmlcaption" class = "nivo-html-caption">';
+    $result .='<strong>This</strong> is an example of a <em>HTML</em> caption with <a href = "#">a link</a>.';
+    $result .='</div>';
+    $result .='</div>';
+    return $result;
+}
 
-  add_action("widgets_init", "np_widgets_init");
+function np_widgets_init() {
+    register_widget('np_Widget');
+}
+ 
+add_action('widgets_init', 'np_widgets_init');
 
-      
-//Widget backend
-
+class np_Widget extends WP_Widget {
+ 
+    public function __construct() {
+        parent::__construct('np_Widget', 'Nivo Slideshow', array('description' => __('A Nivo Slideshow Widget', 'text_domain')));
+    }
 public function form($instance) {
-  if (isset($instance['title'])) {
-      $title = $instance['title'];
+    if (isset($instance['title'])) {
+        $title = $instance['title'];
     }
-  else {
-      $title = __('Widget Slideshow', 'text_domain');
+    else {
+        $title = __('Widget Slideshow', 'text_domain');
     }
     ?>
-<p>
-  <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
-  <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
-</p>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+        </p>
     <?php
+  }
 public function update($new_instance, $old_instance) {
     $instance = array();
     $instance['title'] = strip_tags($new_instance['title']);
@@ -96,7 +100,7 @@ public function widget($args, $instance) {
     echo np_function('np_widget');
     echo $after_widget;
 }
-*/
+}
   ?>
 </body>
 </html> 
