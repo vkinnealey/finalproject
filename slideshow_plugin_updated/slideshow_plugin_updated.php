09ea5595b38e61dde124e12ca082cbb28aefde2b
vkinnealey <victoria.kinnealey@gmail.com>
2017-08-06 20:27:19 -0400

    FINAL upload of child theme and slideshow plugin
    Final code for web dev class

--- slideshow_plugin_updated/slideshow_plugin_updated.php
+++ slideshow_plugin_updated/slideshow_plugin_updated.php
@@ -14,6 +14,7 @@
   </head>
 <body>
 <?php
+//slideshow management functionality
 function np_init() {
   add_shortcode('np-shortcode', 'np_function');
     $args = array(
@@ -27,55 +28,56 @@ function np_init() {
     register_post_type("np_images", $args);
   }
 add_action("init", "np_init");
-
-add_action('wp_print_scripts', 'np_register_scripts');
-add_action('wp_print_styles', 'np_register_styles');
-
+//add Nivo slider features for slide show
+add_action("wp_print_scripts", "np_register_scripts");
+add_action("wp_print_styles", "np_register_styles");
+//formats size of slideshow images 
+/* this code may be impacting featured image downloading ability?
 add_image_size('np_widget', 180, 100, true);
 add_image_size('np_function', 600, 280, true);
 add_theme_support( 'post-thumbnails' );
-  
-function np_function($type='np_function') {
+*/
+//create php movement for slides
+function np_function($type="np_function") {
     $args = array(
-        'post_type' => 'np_images',
-        'posts_per_page' => 5
+        "post_type" => "np_images",
+        "posts_per_page" => 3
     );
     $result = '<div class="slider-wrapper theme-default">';
     $result .= '<div id="slider" class="nivoSlider">';
  
-    //the loop
+//the loop
     $loop = new WP_Query($args);
     while ($loop->have_posts()) {
         $loop->the_post();
- 
-        $the_url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), $type);
+        $the_url = wp_get_attachment_image_src(get_post_thumbnail_id($loop->post->ID), $type);
         $result .='<img title="'.get_the_title().'" src="' . $the_url[0] . '" data-thumb="' . $the_url[0] . '" alt=""/>';
     }
-    $result .= '</div>';
+    $result .= "</div>";
     $result .='<div id = "htmlcaption" class = "nivo-html-caption">';
     $result .='<strong>This</strong> is an example of a <em>HTML</em> caption with <a href = "#">a link</a>.';
     $result .='</div>';
     $result .='</div>';
     return $result;
 }
-
+//widget to actually display slideshow
 function np_widgets_init() {
-    register_widget('np_Widget');
+    register_widget("np_Widget");
 }
  
-add_action('widgets_init', 'np_widgets_init');
+add_action("widgets_init", "np_widgets_init");
 
 class np_Widget extends WP_Widget {
  
     public function __construct() {
-        parent::__construct('np_Widget', 'Nivo Slideshow', array('description' => __('A Nivo Slideshow Widget', 'text_domain')));
+        parent::__construct("np_Widget", "Nivo Slideshow", array("description" => __("A Nivo Slideshow Widget", "text_domain")));
     }
 public function form($instance) {
-    if (isset($instance['title'])) {
-        $title = $instance['title'];
+    if (isset($instance["title"])) {
+        $title = $instance["title"];
     }
     else {
-        $title = __('Widget Slideshow', 'text_domain');
+        $title = __("Widget Slideshow", "text_domain");
     }
     ?>
         <p>
@@ -86,14 +88,13 @@ public function form($instance) {
   }
 public function update($new_instance, $old_instance) {
     $instance = array();
-    $instance['title'] = strip_tags($new_instance['title']);
- 
+    $instance["title"] = strip_tags($new_instance["title"]);
     return $instance;
 }
 public function widget($args, $instance) {
     extract($args);
     // the title
-    $title = apply_filters('widget_title', $instance['title']);
+    $title = apply_filters("widget_title", $instance["title"]);
     echo $before_widget;
     if (!empty($title))
         echo $before_title . $title . $after_title;
